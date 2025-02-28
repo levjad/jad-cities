71046dbb6444309f2acabd2f22db5cf8caf07bec
Lev Jad <levjad@gmail.com>
2025-02-28 11:15:12 +0100

    Refactored backend a bit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CitiesModule } from './cities/cities.module';
 
 @Module({
   imports: [CitiesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

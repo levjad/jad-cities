d935edf10f38829b00ad70bd839ad61321d70e3f
Lev Jad <levjad@gmail.com>
2025-03-02 10:41:53 +0100

    Adjusted e2e test

--- backend/test/cities.e2e-spec.ts
+++ backend/test/cities.e2e-spec.ts
@@ -2,9 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import * as citiesData from '../../data/cities.json';
 
-describe('AppController (e2e)', () => {
+describe('CitiesController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -18,8 +19,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/cities')
       .expect(200)
-      .expect('Hello World!');
+      .expect((citiesData as CitiesData).cities);
   });
 });
